8c9d7f6a2c72b924c5368b295df72d3b7777f419
Narendar115 <narendarkethavath73@gmail.com>
2025-05-13 13:15:27 +0530

    done

--- a6/script.js
+++ a6/script.js
@@ -2,5 +2,5 @@ function greet(){
     var mail=document.getElementById('email').value;
     var password=document.getElementById('password').value;
     
-   document.getElementById('block').innerHTML="mail is "+mail+"<br> password is : "+password;
+   document.getElementById('block').innerHTML="entered mail is  "+mail+"<br> password is : "+password;
 }
