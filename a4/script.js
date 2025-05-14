d2a632bff2f11b825ed62700ef521a0c287ae073
Narendar115 <narendarkethavath73@gmail.com>
2025-05-14 18:48:08 +0530

    update

---
+++ a4/script.js
@@ -0,0 +1,12 @@
+let str="";
+for(var i=1;i<=30;i++){
+    str=str+`<option>${i}</option>`;
+}
+d1.innerHTML=str
+
+
+  let val="";
+  for(var i=2025;i>=2000;i--){
+    val=val+`<option>${i}</option>`;
+  }
+year.innerHTML=val
