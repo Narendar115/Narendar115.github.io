9638059521c0388b2bd92bbf6821bed3b9347168
Narendar115 <narendarkethavath73@gmail.com>
2025-05-14 18:00:19 +0530

    updated

--- a7/script.js
+++ a7/script.js
@@ -1,21 +1,10 @@
-function add1(a,b){
-    return a+b;
-}
-function diff(a,b){
-    return a-b;
-}
-function mul(a,b){
-    return a*b;
-}
-function div(a,b){
-    return a/b;
-}
+
 function add(){
-    let a=parseFloat(document.getElementById('num1').value)
-    let b=parseFloat(document.getElementById('num2').value)
-    let add=add1(a,b);
-    let sub=sub()
-    let mul=mul(a,b);
-    let div=div(a,b);
+    var a=parseFloat(document.getElementById('num1').value)
+    var b=parseFloat(document.getElementById('num2').value)
+    let add=a+b;
+    let sub=a-b;
+    let mul=a*b;
+    let div=a/b;
     document.getElementById('display').innerHTML="addition is "+add+"<br>subtraction is "+sub+" <br>Multiplication is "+mul+" <br>Division is "+div;
 }
