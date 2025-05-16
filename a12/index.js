739a6404a3606f0c93dffe3b3ffced14a5fcb475
Narendar115 <narendarkethavath73@gmail.com>
2025-05-16 19:30:17 +0530

    done

---
+++ a12/index.js
@@ -0,0 +1,8 @@
+const objects=[
+    {id:1,name:"product1",price:20},
+    {id:1,name:"product2",price:30},
+    {id:1,name:"product3",price:40},
+]
+objects.forEach(value=>{
+    console.log(value.name)
+})
