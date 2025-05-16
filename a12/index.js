db9bbb1c0f4943029c3bfd656aefffcdee76ba53
Narendar115 <narendarkethavath73@gmail.com>
2025-05-16 19:38:45 +0530

    done

--- a12/index.js
+++ a12/index.js
@@ -1,8 +1,13 @@
 const objects=[
-    {id:1,name:"product1",price:20},
-    {id:1,name:"product2",price:30},
-    {id:1,name:"product3",price:40},
+    {id:1, name:"product1",price:20},
+    {id:1, name: "product2",price:30},
+    {id:1, name: "product3",price:40},
 ]
-objects.forEach(value=>{
-    console.log(value.name)
+objects.forEach((value)=>{
+    // console.log(value.name)
+    value.price=value.price+5;
+})
+objects.forEach((value)=>{
+    console.log(value.name);
+    console.log(value.price);
 })
