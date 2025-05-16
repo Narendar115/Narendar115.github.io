e06d33aa9cd47e457cffe0662d09bb2bf85f2b58
Narendar115 <narendarkethavath73@gmail.com>
2025-05-16 20:23:45 +0530

    done

--- a13/index.js
+++ a13/index.js
@@ -1,2 +1,4 @@
-fetch("https://jsonplaceholder.typicode.com/users").then((res)=>res.json()
-.then((data)=>console.log(data)));
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((res)=>res.json()
+.then((data)=>console.log(data))
+);
