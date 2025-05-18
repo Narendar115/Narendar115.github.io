24ae5cc8f9b2a45ed19c161fb32138e0094ea818
Narendar115 <narendarkethavath73@gmail.com>
2025-05-18 15:03:22 +0530

    update

--- a12/index.js
+++ a12/index.js
@@ -1,10 +1,22 @@
 const objects=[
-    {id:1, name:"product1",price:20},
-    {id:1, name: "product2",price:30},
-    {id:1, name: "product3",price:40},
+    {id:1, name:"product1",price:20,status:"pending"},
+    {id:1, name: "product2",price:30,status:"pending"},
+    {id:1, name: "product3",price:40,status:"pending"},
 ]
-objects.forEach((value)=>{
-    // console.log(value.name)
-    value.price=value.price+5;
-    console.log(value.name+" "+value.price);
-})
+// objects.forEach((value)=>{
+//     // console.log(value.name)
+//     value.price=value.price+5;
+//     console.log(value.name+" "+value.price);
+// })
+// const updatedobjects=objects.map(item=>({
+//     name:item.name,
+//     price:item.price+5
+// }));
+// updatedobjects.forEach(item=>{
+//     console.log(`Name:${item.name},Price:${item.price}`)
+// });
+const updatedobjects=objects.map((item)=>({...item,price:item.price+5,status:"completed"}));
+// updatedobjects.forEach(item=>{
+//     console.log(`Name:${item.name},Price:${item.price},status:${item.status}`)
+// });
+console.log(updatedobjects);
